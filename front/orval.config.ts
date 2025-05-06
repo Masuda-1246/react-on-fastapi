22da078805378eb8c5cc57f2084b012a4ec6e827
Masuda-1246 <yaojiezengtian38@gmail.com>
2025-05-06 11:47:57 +0900

    orvalの追加

---
+++ front/orval.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "orval";
+
+/**
+ * Orval Configuration
+ * @see https://orval.dev/reference/configuration/overview
+ */
+export default defineConfig({
+  petstore: {
+    input: {
+      target: "../backend/openapi.json",
+    },
+    output: {
+      mode: "split",
+      target: "./src/api/generated",
+      schemas: "./src/api/model",
+      client: "react-query",
+      clean: true,
+      override: {
+        query: {
+          useQuery: true,
+          useInfinite: false,
+        },
+      },
+    },
+  },
+});
